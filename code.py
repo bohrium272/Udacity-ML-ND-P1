58bf7c85b4b711cb38685ac7f81b6fb4003d1d45
Arpit Gogia <arpitgogia@protonmail.com>
2016-06-26 17:55:25 +0530

    Initial Commit

---
+++ code.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+import visuals as vs # Supplementary code
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.metrics import r2_score, make_scorer
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.grid_search import GridSearchCV
+# Pretty display for notebooks
+#%matplotlib inline
+
+# Load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MDEV']
+features = data.drop('MDEV', axis = 1)
+
+X_train, X_test, y_train, y_test = train_test_split(features, prices, test_size=0.2, train_size=0.8, random_state=42)
+print "Training and testing split was successful."
+print "Boston housing dataset has {} data points with {} variables each.".format(*data.shape)
+# TODO: Minimum price of the data
+minimum_price = np.min(prices)
+
+# TODO: Maximum price of the data
+maximum_price = np.max(prices)
+
+# TODO: Mean price of the data
+mean_price = np.mean(prices)
+
+# TODO: Median price of the data
+median_price = np.median(prices)
+
+# TODO: Standard deviation of prices of the data
+std_price = np.std(prices)
+
+# Show the calculated statistics
+print "Statistics for Boston housing dataset:\n"
+print "Minimum price: ${:,.2f}".format(minimum_price)
+print "Maximum price: ${:,.2f}".format(maximum_price)
+print "Mean price: ${:,.2f}".format(mean_price)
+print "Median price ${:,.2f}".format(median_price)
+print "Standard deviation of prices: ${:,.2f}".format(std_price)
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between 
+        true and predicted values based on the metric chosen. """
+    
+    # TODO: Calculate the performance score between 'y_true' and 'y_predict'
+    score = r2_score(y_true, y_predict)
+    
+    # Return the score
+    return score
+
+score = performance_metric([3, -0.5, 2, 7, 4.2], [2.5, 0.0, 2.1, 7.8, 5.3])
+print "Model has a coefficient of determination, R^2, of {:.3f}.".format(score)
+#vs.ModelComplexity(features, prices)
+#input()
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a 
+        decision tree regressor trained on the input data [X, y]. """
+    
+    # Create cross-validation sets from the training data
+    cv_sets = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.20, random_state = 0)
+
+    # TODO: Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # TODO: Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    params = {'max_depth':[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+
+    # TODO: Transform 'performance_metric' into a scoring function using 'make_scorer' 
+    scoring_fnc = make_scorer(performance_metric)
+
+    # TODO: Create the grid search object
+    grid = GridSearchCV(regressor, params, scoring = scoring_fnc, cv = cv_sets)
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
+# Fit the training data to the model using grid search
+reg = fit_model(X_train, y_train)
+
+# Produce the value for 'max_depth'
+print "Parameter 'max_depth' is {} for the optimal model.".format(reg.get_params()['max_depth'])
+client_data = [[5, 34, 15], # Client 1
+               [4, 55, 22], # Client 2
+               [8, 7, 12]]  # Client 3
+
+# Show predictions
+for i, price in enumerate(reg.predict(client_data)):
+    print "Predicted selling price for Client {}'s home: ${:,.2f}".format(i+1, price)
+vs.PredictTrials(features, prices, fit_model, client_data)
